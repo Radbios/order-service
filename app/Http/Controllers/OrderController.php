e68e2c1fb9b9425c4d026ff8de19d904492570c0
Fabiano <radbios@gmail.com>
2025-05-08 04:28:05 -0300

    update

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Auth;
+use App\Models\Order;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class OrderController extends Controller
+{
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'items' => ['required', 'array', 'min:1'],
+            'items.*.product_id' => ['required', 'integer'],
+            'items.*.quantity' => ['required', 'integer', 'min:1'],
+            'shipping_cost' => ['required', 'numeric', 'min:0'],
+            'shipping_service' => ['required', 'string', 'max:100'],
+            'payment_type' => ["required"]
+        ]);
+
+        $items = $request->input('items'); // [{product_id, quantity}]
+        $shippingCost = $request->input('shipping_cost', 0);
+
+        $total = 0;
+
+        $order = Order::create([
+            'user_id' => Auth::user()->id,
+            'payment_type' => $request->input("payment_type"),
+            'status' => 'pending',
+            'shipping_service' => $request->input('shipping_service'),
+            'shipping_cost' => $shippingCost,
+            'total' => 0, // temporariamente
+        ]);
+
+        foreach ($items as $item) {
+            $response = Http::get(env("APP_GATEWAY") . '/catalog/products/' . $item['product_id']);
+    
+            $product = $response->json('data');
+
+            $subtotal = $product['price'] * $item['quantity'];
+            $total += $subtotal;
+
+            $order->items()->create([
+                'product_id' => $product['id'],
+                'quantity' => $item['quantity'],
+                'unit_price' => $product['price'],
+            ]);
+        }
+
+        $order->update(['total' => $total + $shippingCost]);
+
+        return response()->json(['order' => $order->load('items')], 201);
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,15 @@
 <?php
 
 use App\Auth;
+use App\Http\Controllers\OrderController;
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware("api_auth")->group(function() {
+    Route::post("order", [OrderController::class, "store"]);
+    
     Route::get("test", function(Request $request) {
         return Auth::user()->id;
     });
